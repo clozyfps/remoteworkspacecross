265604953cdab4c73c785238b117d05242a59f75
jayso <jayso@10.0.0.21>
2022-08-17 11:53:02 -0400

    MCreator Merge commit 'b2a45d1b92d6e5a2b55b3482bef675759d9677f9' into 64b3a788f3573fad999f47065e1a6dce30337254

--- src/main/java/net/mcreator/animecross/init/AnimecrossModModels.java
+++ src/main/java/net/mcreator/animecross/init/AnimecrossModModels.java
@@ -9,6 +9,9 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.api.distmarker.Dist;
 
+import net.mcreator.animecross.client.model.Modelslime;
+import net.mcreator.animecross.client.model.Modelodm;
+
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = {Dist.CLIENT})
 public class AnimecrossModModels {
 	@SubscribeEvent
